227e1ce8144d12ff4a1c0bccdd83e04426359fc0
MrSofty123 <shaheer1642@gmail.com>
2023-05-28 04:20:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,11 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.use((err, req, res, next) => {
-  console.log(err)
-  if (err) {
-    res.status(400).send('error parsing data')
-  } else {
-    next()
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    console.error(JSON.stringify(err));
+    return res.status(400).send({ status: 404, message: err.message }); // Bad request
   }
+  next()
 })
 
 app.get('/', (req, res) => {
