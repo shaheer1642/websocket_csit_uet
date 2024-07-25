a361f15663ac828de9bbc04aafc7824313efb479
shaheer1642@gmail.com <shaheer1642@gmail.com>
2024-07-25 15:03:40 +0500

    users endpoint

--- modules/email_code_verification.js
+++ modules/email_code_verification.js
@@ -3,8 +3,8 @@ const { users } = require("./objects/users")
 
 const emailVerificationCodes = {}
 
-function emailVerificationCode(user_id,user_email) {
-    return new Promise((resolve,reject) => {
+function emailVerificationCode(user_id, user_email) {
+    return new Promise((resolve, reject) => {
         if (!user_email) user_email = users[user_id].user_email
         if (!user_email) return reject('Could not find email to send verification code')
 
@@ -16,7 +16,7 @@ function emailVerificationCode(user_id,user_email) {
             `Please enter the code ${code}\nThis code will expire in 5 minutes`,
             user_email
         ).then(res => {
-            setVerificationCode(user_id,code)
+            setVerificationCode(code, user_id)
             resolve()
         }).catch(err => {
             reject(err)
@@ -24,31 +24,31 @@ function emailVerificationCode(user_id,user_email) {
     })
 }
 
-function timeoutVerificationCode(code,timeout_duration = 300000) {
+function timeoutVerificationCode(code, timeout_duration = 300000) {
     setTimeout(() => {
         delete emailVerificationCodes[code]
     }, timeout_duration);
 }
 
-function setVerificationCode(user_id,code) {
+function setVerificationCode(code, user_id) {
     emailVerificationCodes[code] = user_id
     timeoutVerificationCode(code)
 }
 
-function verifyVerificationCode(user_id,code) {
-    if (emailVerificationCodes[code] && emailVerificationCodes[code] == user_id) {
-        timeoutVerificationCode(code,1)
-        return true
+function verifyVerificationCode(code) {
+    if (emailVerificationCodes[code]) {
+        timeoutVerificationCode(code, 1)
+        return emailVerificationCodes[code]
     } else {
-        return false
+        return null
     }
 }
 
 function generateVerificationCode() {
     let ID = "";
     let characters = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
-    for ( var i = 0; i < 6; i++ ) {
-      ID += characters.charAt(Math.floor(Math.random() * characters.length));
+    for (var i = 0; i < 6; i++) {
+        ID += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return ID;
 }
--- routes/users.js
+++ routes/users.js
@@ -3,32 +3,12 @@ const router = express.Router()
 const db = require('../modules/db')
 const passport = require('../modules/passport')
 const jwt = require('jsonwebtoken');
-const { validateData } = require('../modules/validator');
+const { validateData, isBase64 } = require('../modules/validator');
 const { body, query } = require('express-validator');
 const { sendMail } = require('../modules/gmail_client');
 const { hashPassword } = require('../modules/hashing');
-
-router.get('/users', passport.authenticate('jwt'), (req, res) => {
-    db.query(`
-        SELECT * FROM users WHERE user_type NOT IN ('student','teacher');
-        SELECT * FROM students;
-        SELECT * FROM teachers;
-    `).then(db_res => {
-        const users_list = []
-
-        db_res[0].rows.concat(db_res[1].rows.concat(db_res[2].rows)).forEach(user => {
-            users_list.push({
-                user_id: user.user_id || user.student_id || user.teacher_id,
-                name: user.student_name || user.teacher_name || user.user_type,
-            })
-        })
-
-        return res.send(users_list)
-    }).catch(err => {
-        console.error(err)
-        res.status(500).send(err.message || err.detail || JSON.stringify(err))
-    })
-});
+const { uploadFile } = require('../modules/aws/aws');
+const { verifyVerificationCode, emailVerificationCode } = require('../modules/email_code_verification');
 
 router.get('/user', passport.authenticate('jwt'), (req, res) => {
     console.log('GET /user')
@@ -50,7 +30,9 @@ router.get('/user', passport.authenticate('jwt'), (req, res) => {
     `).then((db_res) => {
             if (db_res.rowCount == 1) {
                 const user = db_res.rows[0]
-                user.user_id = user.user_id || user.student_id || user.teacher_id
+                user.user_id = (user.user_id || user.student_id || user.teacher_id);
+                user.name = (user.student_name || user.teacher_name || user.user_type)
+
                 delete user.password
                 delete user.jwt_token
                 return res.send(user)
@@ -72,107 +54,185 @@ router.post('/user/login', (req, res, next) => {
         const token = jwt.sign({ sub: user.user_id }, process.env.JWT_SECRET);
         // Store the token in the database for session persistence
         db.query('UPDATE users SET jwt_token = ($2) WHERE user_id = ($1)', [user.user_id, token])
-            .then((db_res) => res.send({ token })).catch(err => next(err));
+            .then((db_res) => res.send({ token }))
+            .catch(err => next(err));
     })(req, res, next);
 });
 
-// router.post('/user/changePassword',
-//     passport.authenticate('jwt'),
-//     (req, res, next) => validateData([
-//         body('old_password').isString().withMessage('Password is required'),
-//         body('password').isString().withMessage('Password is required'),
-//     ], req, res, next),
-//     (req, res) => {
-//         const user = req.user
-//         const body = req.body
+router.patch('/user/updateAvatar',
+    passport.authenticate('jwt'),
+    (req, res, next) => validateData([
+        body('avatar').custom(isBase64).withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`),
+    ], req, res, next),
+    async (req, res, next) => {
+        const data = { ...req.user, ...req.body }
 
-//         if (hashPassword(body.old_password) != user.password) return res.status(400).send('Old password is incorrect')
+        const fileUrl = await uploadFile('avatar', data.avatar).catch(console.error)
+        if (!fileUrl) return res.status(500).send('Error uploading file')
 
-//         db.query(`
-//             UPDATE users SET password = ($2) WHERE user_id = ($1)
-//         `, [user.user_id, hashPassword(body.password)]).then(db_res => {
-//             if (db_res.rowCount == 1) res.send("Password changed successfully")
-//             else if (db_res.rowCount == 0) res.sendStatus(404)
-//             else res.status(500).send(`Unexpected DB response. Updated ${db_res.rowCount} rows`)
-//         }).catch(err => {
-//             console.error(err)
-//             res.status(500).send(err.message || err.detail || JSON.stringify(err))
-//         })
-//     }
-// )
+        db.query(`
+            UPDATE users SET avatar = '${fileUrl}' WHERE user_id = '${data.user_id}';
+        `).then(db_res => {
+            if (db_res.rowCount == 1) res.send('Avatar updated')
+            else res.sendStatus(404)
+        }).catch(err => {
+            console.error(err)
+            res.status(500).send(err.message || err.detail || JSON.stringify(err))
+        })
+    }
+);
 
+router.patch('/user/updateEmail',
+    (req, res, next) => validateData([
+        body('user_email').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`),
+        body('email_verification_code').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`),
+    ], req, res, next),
+    async (req, res, next) => {
+        const data = req.body
 
-// router.post('/user/resetPassword',
-//     (req, res, next) => validateData([
-//         body('code').isInt().isLength({ min: 6, max: 6 }).withMessage('Invalid code'),
-//         body('password').isString().withMessage('Password is required'),
-//     ], req, res, next),
-//     (req, res) => {
-//         const { code, password } = req.body
+        data.user_id = verifyVerificationCode(data.email_verification_code)
+        if (!data.user_id) return res.status(400).send('Invalid verification code')
 
-//         const user = emailOTPs.find(o => o.code == code)
-//         if (!user) return res.status(400).send('Invalid OTP code. Please try again')
-//         const user_id = user.user_id
+        db.query(`
+                UPDATE users SET 
+                user_email = '${data.user_email}'
+                WHERE user_id = '${data.user_id}';
+            `).then(db_res => {
+            if (db_res.rowCount == 1) res.send('Email updated')
+            else res.status(500).send('Unexpected DB error')
+        }).catch(err => {
+            console.error(err)
+            res.status(500).send(err.message || err.detail || JSON.stringify(err))
+        })
+    }
+);
 
-//         db.query(`
-//             UPDATE users SET password = ($2) WHERE user_id = ($1)
-//         `, [user_id, hashPassword(password)]).then(db_res => {
-//             if (db_res.rowCount == 1) res.send("Password reset successfully")
-//             else if (db_res.rowCount == 0) res.sendStatus(404)
-//             else res.status(500).send(`Unexpected DB response. Updated ${db_res.rowCount} rows`)
-//         }).catch(err => {
-//             console.error(err)
-//             res.status(500).send(err.message || err.detail || JSON.stringify(err))
-//         })
-//     }
-// )
+router.post('/user/sendEmailVerificationCode',
+    (req, res, next) => validateData([
+        body('user_id').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`).optional(),
+        body('user_type').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`).optional(),
+        body('user_email').isEmail().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`).optional(),
+        body('username').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`).optional(),
+    ], req, res, next),
+    async (req, res, next) => {
+        const data = req.body
 
-// var emailOTPs = []
-// router.post('/user/emailOTP',
-//     (req, res, next) => validateData([
-//         body('email').isEmail().withMessage('Email is required'),
-//     ], req, res, next),
-//     (req, res) => {
-//         const email = req.body.email?.toLowerCase()
+        if (data.username) {
+            if (!['admin', 'pga'].includes(data.username) && !data.user_type) return res.status(400).send('user_type not provided with the username')
+            db.query(`
+                SELECT * FROM users WHERE 
+                username = '${data.username}'
+                ${['admin', 'pga'].includes(data.username) ? '' : `AND user_type = '${data.user_type}'`};
+            `).then(db_res => {
+                const user = db_res.rows[0]
+                if (!user) return res.status(400).send('No user registered with given username and user type')
+                emailVerificationCode(user.user_id, user.user_email).then(() => {
+                    return res.send('email sent')
+                }).catch(err => {
+                    return res.status(500).send(err.message || err.detail || JSON.stringify(err))
+                })
+            }).catch(err => {
+                console.error(err)
+                return res.status(500).send(err.message || err.detail || JSON.stringify(err))
+            })
+        } else if (data.user_email && data.user_id) {
+            emailVerificationCode(data.user_id, data.user_email).then(() => {
+                return res.send('email sent')
+            }).catch(err => {
+                return res.status(500).send(err.message || err.detail || JSON.stringify(err))
+            })
+        } else if (data.user_email) {
+            db.query(`
+                SELECT * FROM users WHERE user_email = '${data.user_email}'
+            `).then(db_res => {
+                const user = db_res.rows[0]
+                if (!user) return res.status(400).send('No user registered with given email')
+                emailVerificationCode(user.user_id, user.user_email).then(() => {
+                    return res.send('email sent')
+                }).catch(err => {
+                    return res.status(500).send(err.message || err.detail || JSON.stringify(err))
+                })
+            }).catch(err => {
+                console.error(err)
+                return res.status(500).send(err.message || err.detail || JSON.stringify(err))
+            })
+        } else {
+            return res.status(400).send('No username or email provided')
+        }
+    }
+);
 
-//         db.query(`SELECT * FROM users WHERE LOWER(email) = '${email}'`)
-//             .then(db_res => {
-//                 if (db_res.rowCount == 1) {
-//                     const { user_id } = db_res.rows[0]
-//                     const code = Math.floor(Math.random() * 900000) + 100000
-//                     emailOTPs.push({ code, email, user_id })
-//                     setTimeout(() => { emailOTPs = emailOTPs.filter(o => o.code != code) }, 600000);
-//                     sendMail({
-//                         to: email,
-//                         title: 'Reset Password Request',
-//                         body: `We have received a request for resetting your account password\n\nYour verification code is ${code}\nThis code will expire in the next 10 minutes\n\nIf this was not you, please ignore this email`
-//                     }).then(() => {
-//                         res.send('Verification code has been sent to your email')
-//                     }).catch(err => {
-//                         console.error(err)
-//                         res.status(500).send('Error sending email: ' + err.message || err)
-//                     })
-//                 } else if (db_res.rowCount == 0) res.status(404).send('That email does not exist')
-//                 else res.status(500).send(`Unexpected DB response. Updated ${db_res.rowCount} rows`)
-//             }).catch(err => {
-//                 console.error(err)
-//                 res.status(500).send(err.message || err.detail || JSON.stringify(err))
-//             })
-//     }
-// );
+router.patch('/user/changePassword',
+    passport.authenticate('jwt'),
+    (req, res, next) => validateData([
+        body('current_password').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`),
+        body('new_password').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`),
+    ], req, res, next),
+    (req, res) => {
+        const data = { ...req.user, ...req.body }
 
-// router.get('/user/emailOTP/verify',
-//     (req, res, next) => validateData([
-//         query('code').isInt().isLength({ min: 6, max: 6 }).withMessage('Invalid code'),
-//     ], req, res, next),
-//     (req, res) => {
-//         console.log(emailOTPs)
-//         const { code } = req.query
-//         if (!emailOTPs.find(o => o.code == code))
-//             return res.status(400).send('Invalid code')
-//         else
-//             return res.send('Code is valid')
-//     }
-// );
+        if (data.password !== hashPassword(data.current_password)) return res.status(400).send('Current password is incorrect')
+
+        db.query(`
+            UPDATE users SET password = '${hashPassword(data.new_password)}' WHERE user_id = '${data.user_id}';
+        `).then(db_res => {
+            if (db_res.rowCount == 1) res.send('Password updated')
+            else res.sendStatus(404)
+        }).catch(err => {
+            console.error(err)
+            res.status(500).send(err.message || err.detail || JSON.stringify(err))
+        })
+    }
+)
+
+router.patch('/user/resetPassword',
+    (req, res, next) => validateData([
+        body('email_verification_code').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`),
+        body('new_password').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`),
+    ], req, res, next),
+    (req, res) => {
+        const data = req.body
+
+        data.user_id = verifyVerificationCode(data.email_verification_code)
+        if (!data.user_id) return res.status(400).send('Invalid verification code')
+
+        db.query(`
+            UPDATE users SET password = '${hashPassword(data.new_password)}' WHERE user_id = '${data.user_id}';
+        `).then(db_res => {
+            if (db_res.rowCount == 1) res.send('Password reset successful')
+            else res.status(500).send('Unexpted DB error')
+        }).catch(err => {
+            console.error(err)
+            res.status(500).send(err.message || err.detail || JSON.stringify(err))
+        })
+    }
+)
+
+router.patch('/user/updateFCMToken',
+    passport.authenticate('jwt'),
+    (req, res, next) => validateData([
+        body('fcm_token').isString().notEmpty().withMessage((value, { path }) => `Invalid value "${value}" provided for field "${path}"`),
+    ], req, res, next),
+    (req, res) => {
+        const data = { ...req.user, ...req.body }
+
+        if (data.fcm_tokens.find(ft => ft.token == data.fcm_token)) return res.status(400).send('FCM token already exists')
+
+        data.fcm_tokens.push({ timestamp: new Date().getTime(), token: data.fcm_token })
+        while (data.fcm_tokens.length > 5) data.fcm_tokens.shift()
+
+        db.query(`
+            UPDATE users SET
+            fcm_tokens = '${JSON.stringify(data.fcm_tokens)}'
+            WHERE user_id = '${data.user_id}'
+        `).then(db_res => {
+            if (db_res.rowCount == 1) res.send('updated token')
+            else res.status(400).send('Unexpected DB error')
+        }).catch(err => {
+            console.error(err)
+            res.status(500).send(err.message || err.detail || JSON.stringify(err))
+        })
+    }
+)
 
 module.exports = router
